a4a2d21037fc3769ddcf1ac505517247fe26c2fd
linusidom <linusidom@gmail.com>
2022-08-25 03:42:08 +0700

    fixed circleci

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   useEffect(()=> {
     dispatch(robotSliceActions.loadRobots())
-  },[])
+  },[dispatch])
 
 
   return (
--- src/MyCard.js
+++ src/MyCard.js
@@ -2,7 +2,7 @@ export const MyCard = (props) => {
     // console.log(props)
     return(
         <div className="mycard shadow col-3">
-            <img src={`https://robohash.org/${props.id}?size=200x200`} alt='picture'/>
+            <img src={`https://robohash.org/${props.id}?size=200x200`} alt='robot_person'/>
             <h2>{props.name}</h2>
             <p>{props.email}</p>
         </div>
